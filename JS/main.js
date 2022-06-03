4dc224695790414fad6f4c2d4bf68d0aa7b956d6
Stevent Florez <steventflorez94@gmail.com>
2022-06-03 03:10:38 +0200

    se agregan preguntas

--- JS/main.js
+++ JS/main.js
@@ -3,70 +3,133 @@
  } from "./preguntas.js";
 
  const container = document.querySelector('.container');
-
-
+ let puntaje =parseInt(localStorage.getItem('puntaje')) 
+ let nivelIndicador = 35
  const nivel = {
      nivel: 1
  }
 
 
+ if( puntaje == null){
+     localStorage.setItem('puntaje',0)
+    puntaje = parseInt(localStorage.getItem('puntaje')) 
+ }
+ 
+
+
+
 
  export class UI {
 
+    static cambioPreguntas(btn){
+        UI.cambiarDisplay(btn);
+        UI.cambiarcolor(5)
+
+     preguntaActual = Datos.elegirpregunta()
+     console.log(preguntaActual)
+     respuestas = Object.values(preguntaActual);
+     titulo.textContent = UI.mostrarPregunta(preguntaActual)
+     btn_a.textContent = respuestas[1];
+     btn_b.textContent = respuestas[2];
+     btn_c.textContent = respuestas[3];
+     btn_d.textContent = respuestas[4];
+
+    }
+
+     static cambiarcolor(r, btn, correcta) {
+         if (r) {
+
+             switch (btn) {
+                 case 1:
+                     btn_a.className = 'btn btn-success btn_r'
+                     UI.cambiarDisplay(true)
+                     break
+                 case 2:
+                     btn_b.className = 'btn btn-success btn_r'
+                     UI.cambiarDisplay(true)
+                     break
+                 case 3:
+                     btn_c.className = 'btn btn-success btn_r'
+                     UI.cambiarDisplay(true)
+                     break
+                 case 4:
+                     UI.cambiarDisplay(true)
+                     btn_d.className = 'btn btn-success btn_r'
+
+                     break
+                 default:
+                     btn_a.className = 'btn btn-outline-warning btn_r'
+                     btn_b.className = 'btn btn-outline-warning btn_r'
+                     btn_c.className = 'btn btn-outline-warning btn_r'
+                     btn_d.className = 'btn btn-outline-warning btn_r'
+                     break
 
 
-     static crearDom(datos) {
+             }
+         } else {
+             UI.cambiarDisplay(false)
+             UI.mostrarCorrecta(correcta)
+             switch (btn) {
+                 case 1:
+                     btn_a.className = 'btn btn-danger btn_r'
 
-         const divNiveles = document.createElement('div');
-         const divPregunta = document.createElement('div');
-         const divpreguntas = document.createElement('div')
-         const titulo = document.createElement('h2')
-         const preguntaActual = Datos.elegirpregunta()
-         const divContainerNibeles = this.recorrerNiveles(datos);
-         const respuestas = Object.values(preguntaActual);
-         const contenedorRespuestas = document.createElement('div');
-         const btn_a = document.createElement('button');
-         const btn_b = document.createElement('button');
-         const btn_c = document.createElement('button');
-         const btn_d = document.createElement('button');
-         const btn_siguiente = document.createElement('button');
-         const btn_nuevo_intento = document.createElement('button');
-         btn_nuevo_intento.className = 'btn btn-danger'
-         btn_nuevo_intento.textContent = 'Intentalo nuevamente'
-         btn_nuevo_intento.style.display = 'none'
-         btn_siguiente.className = 'btn btn-info'
-         btn_siguiente.textContent = 'Siquiente Pregunta'
-         btn_siguiente.style.display = 'none'
-         container.className = 'row container'
-         divNiveles.className = 'col-3';
-         divPregunta.className = 'col-9';
-         divpreguntas.className = 'card border-success mb-3 containerPreguntas';
-         titulo.textContent = this.mostrarPregunta(preguntaActual)
-         divpreguntas.appendChild(titulo)
-         divPregunta.appendChild(divpreguntas)
-         divNiveles.appendChild(divContainerNibeles);
-         container.appendChild(divNiveles);
-         container.appendChild(divPregunta);
-         btn_a.className = 'btn btn-outline-warning btn_r'
-         btn_b.className = 'btn btn-outline-warning btn_r'
-         btn_c.className = 'btn btn-outline-warning btn_r'
-         btn_d.className = 'btn btn-outline-warning btn_r'
-         btn_a.textContent = respuestas[1];
-         btn_b.textContent = respuestas[2];
-         btn_c.textContent = respuestas[3];
-         btn_d.textContent = respuestas[4];
-         divpreguntas.append(btn_a, btn_b, btn_c, btn_d,btn_siguiente,btn_nuevo_intento)
-         divpreguntas.appendChild(contenedorRespuestas)
+                     break
+                 case 2:
+                     btn_b.className = 'btn btn-danger btn_r'
 
+                     break
+                 case 3:
+                     btn_c.className = 'btn btn-danger btn_r'
 
+                     break
+                 default:
+                     btn_d.className = 'btn btn-danger btn_r'
 
+                     break
 
-
+             }
+         }
      }
 
-    static cambiarcolor (e){
-        console.log( e.target)
-    }
+     static mostrarCorrecta(correcta){
+        switch (correcta) {
+            case 1:
+                btn_a.className = 'btn btn-success disabled btn_r'
+
+                break
+            case 2:
+                btn_b.className = 'btn btn-success disabled btn_r'
+
+                break
+            case 3:
+                btn_c.className = 'btn btn-success disabled btn_r'
+
+                break
+            default:
+                btn_d.className = 'btn btn-success disabled btn_r'
+
+                break
+
+        }
+
+     }
+     static cambiarDisplay(btn) {
+
+         if (btn) {
+
+             if (btn_siguiente.style.display == 'none') {
+                 btn_siguiente.style.display = 'block'
+             } else {
+                 btn_siguiente.style.display = 'none'
+             }
+         } else {
+             if (btn_nuevo_intento.style.display == 'none') {
+                 btn_nuevo_intento.style.display = 'block'
+             } else {
+                 btn_nuevo_intento.style.display = 'none'
+             }
+         }
+     }
 
      static mostrarPregunta(pregunta) {
          return pregunta.pregunta
@@ -82,7 +145,7 @@
 
 
          for (let i = 0; i < numNiveles; i++) {
-             console.log(i)
+
              const divNivel = document.createElement('div');
              divNivel.className = 'col-12 niveles'
              const h4Nivel = document.createElement('h4')
@@ -97,9 +160,17 @@
 
      }
 
-     static cargarDom() {
-         console.log('hello')
+     static moverIndicador(e){
+        if(e){
+            nivelIndicador+=35
+            indicador.style.top = `${nivelIndicador}px`
+        }else{
+            nivelIndicador = 35
+            indicador.style.top = `${nivelIndicador}px`
+        }
      }
+
+
  }
 
  export class Datos {
@@ -125,24 +196,140 @@
          if (nivel.nivel == 1) {
              return preguntas[0].nivel1
 
-         }else{
-            return preguntas[0].nivel2
+         } else {
+             return preguntas[0].nivel2
          }
      }
 
-     static comprobarRespuesta(respuesta){
-        console.log(respuesta)
+     static comprobarRespuesta(respuesta) {
+         const correcta = Object.values(preguntaActual)[5]
+         if (respuesta == correcta) {
+             UI.cambiarcolor(true, respuesta, correcta);
+         } else {
+             UI.cambiarcolor(false, respuesta, correcta);
+         }
      }
+
+     static sumarPuntaje(){
+         puntaje+=5
+         localStorage.setItem('puntaje', puntaje)
+         console.log(localStorage.getItem('puntaje'))
+         historiaPuntaje.textContent = `Puntaje Acomulado: ${puntaje}`
+
+
+     }
+     static restaurarjuego (){
+         
+     }
+
+     static restarPuntaje(){
+        localStorage.setItem('puntaje', 0)
+        puntaje = parseInt(localStorage.getItem('puntaje'))
+        historiaPuntaje.textContent = `Puntaje Acomulado: ${puntaje}`
+     }
+
+     
  }
 
 
-  
+ const divNiveles = document.createElement('div');
+ const divPregunta = document.createElement('div');
+ const divpreguntas = document.createElement('div')
+ const titulo = document.createElement('h2')
+ let preguntaActual = Datos.elegirpregunta()
+ let divContainerNibeles = UI.recorrerNiveles(preguntas);
+ let respuestas = Object.values(preguntaActual);
+ const contenedorRespuestas = document.createElement('div');
+ let btn_a = document.createElement('button');
+ let btn_b = document.createElement('button');
+ let btn_c = document.createElement('button');
+ let btn_d = document.createElement('button');
+ const btn_siguiente = document.createElement('button');
+ const btn_nuevo_intento = document.createElement('button');
+ const containerUsuario = document.createElement('div');
+ let historiaPuntaje = document.createElement('h2');
+ const indicador = document.createElement('div')
+ indicador.className = 'indicador';  
+historiaPuntaje.textContent = `Puntaje Acomulado: ${puntaje}`
+containerUsuario.className = 'col-12 card border-info mb-3 containerPuntaje'
+ btn_nuevo_intento.className = 'btn btn-secondary'
+ btn_nuevo_intento.textContent = 'Intentalo nuevamente'
+ btn_nuevo_intento.style.display = 'none'
+ btn_siguiente.className = 'btn btn-info'
+ btn_siguiente.textContent = 'Siquiente Pregunta'
+ btn_siguiente.style.display = 'none'
+ container.className = 'row container'
+ divNiveles.className = 'col-3';
+ divPregunta.className = 'col-9';
+ divpreguntas.className = 'card border-success mb-3 containerPreguntas';
+ titulo.textContent = UI.mostrarPregunta(preguntaActual)
+ divpreguntas.appendChild(titulo)
+ divPregunta.appendChild(divpreguntas)
+ divNiveles.appendChild(indicador)
+ divNiveles.appendChild(divContainerNibeles);
+ containerUsuario.appendChild(historiaPuntaje);
+ container.appendChild(containerUsuario);
+ container.appendChild(divNiveles);
+ container.appendChild(divPregunta);
+ btn_a.className = 'btn btn-outline-warning btn_r'
+ btn_b.className = 'btn btn-outline-warning btn_r'
+ btn_c.className = 'btn btn-outline-warning btn_r'
+ btn_d.className = 'btn btn-outline-warning btn_r'
+ btn_a.textContent = respuestas[1];
+ btn_b.textContent = respuestas[2];
+ btn_c.textContent = respuestas[3];
+ btn_d.textContent = respuestas[4];
+ divpreguntas.append(btn_a, btn_b, btn_c, btn_d, btn_siguiente, btn_nuevo_intento)
+ divpreguntas.appendChild(contenedorRespuestas)
 
 
- document.addEventListener('DOMContentLoaded', UI.crearDom(preguntas));
 
 
- btn_a.addEventListener('click',comprobarRespuesta(1) );
- btn_b.addEventListener('click',comprobarRespuesta(2) );
- btn_c.addEventListener('click',comprobarRespuesta(3) );
- btn_d.addEventListener('click',comprobarRespuesta(4) ); 
+
+
+
+
+ btn_a.addEventListener('click', (e) => {
+     Datos.comprobarRespuesta(1)
+ });
+
+ btn_b.addEventListener('click', (e) => {
+     Datos.comprobarRespuesta(2)
+ });
+
+ btn_c.addEventListener('click', (e) => {
+     Datos.comprobarRespuesta(3)
+ });
+
+ btn_d.addEventListener('click', (e) => {
+     Datos.comprobarRespuesta(4)
+ });
+
+ btn_siguiente.addEventListener('click', (e) => {
+
+     nivel.nivel++
+
+     if (nivel.nivel <= 5) {
+         UI.cambioPreguntas(true)
+         UI.moverIndicador(true)
+
+     }else{
+         nivel.nivel = 1
+         UI.moverIndicador(false)
+         UI.cambioPreguntas(true)
+         Datos.sumarPuntaje()
+         
+     }
+ })
+
+ btn_nuevo_intento.addEventListener('click', (e)=>{
+
+     nivel.nivel = 1
+     UI.moverIndicador(false)
+     UI.cambioPreguntas(false)
+     Datos.restarPuntaje()
+
+ })
+ /*  btn_b.addEventListener('click',  Datos.comprobarRespuesta(2) );
+  btn_c.addEventListener('click', Datos.comprobarRespuesta(3) );
+  btn_d.addEventListener('click', Datos.comprobarRespuesta(4) );  */
--- JS/preguntas.js
+++ JS/preguntas.js
@@ -2,48 +2,48 @@ export const preguntas = [{
 
     nivel1: {
         pregunta1: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "25",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: 1
-
-        },
-        pregunta2: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "25",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: 2
-
-        },
-        pregunta3: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "25",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
+            pregunta: "¿Cuantos dias tiene un año?",
+            respiesta1: "100",
+            respiesta2: "563",
+            respiesta3: "365",
+            respiesta4: "265",
             correcta: 3
 
         },
+        pregunta2: {
+            pregunta: "¿En que pais esta la muralla china?",
+            respiesta1: "Japon",
+            respiesta2: "Cuba",
+            respiesta3: "China",
+            respiesta4: "Roma",
+            correcta: 3
+
+        },
+        pregunta3: {
+            pregunta: "¿Cual de los siguientes animales no puede volar?",
+            respiesta1: "Aguila",
+            respiesta2: "Condor",
+            respiesta3: "Paloma",
+            respiesta4: "Gallina",
+            correcta: 4
+
+        },
         pregunta4: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "25",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: 2
+            pregunta: "¿como se dice CARRO en ingles?",
+            respiesta1: "Rum",
+            respiesta2: "Cat",
+            respiesta3: "Car",
+            respiesta4: "Bus",
+            correcta: 3
 
         },
         pregunta5: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "25",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuales son los colores de la bandera de Colombia?",
+            respiesta1: "Azul, Verda y Rojo",
+            respiesta2: "Amarillo, Azul y Rojo",
+            respiesta3: "Rojo, Morado y Dorado",
+            respiesta4: "Amarillo Azul y rosado",
+            correcta: 2
 
         }
 
@@ -52,48 +52,48 @@ export const preguntas = [{
     },
     nivel2: {
         pregunta1: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "1",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuál es el animal más grande de la Tierra?",
+            respiesta1: "La ballena azul",
+            respiesta2: "El elefante",
+            respiesta3: "La jirafa",
+            respiesta4: "El cocodrilo",
+            correcta: 1
 
         },
         pregunta2: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "2",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuál es el planeta más alejado del Sol?",
+            respiesta1: "Jupiter",
+            respiesta2: "Neptuno",
+            respiesta3: "Pluton",
+            respiesta4: "Marte",
+            correcta: 2
 
         },
         pregunta3: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "3",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuántos días tiene un año bisiesto?",
+            respiesta1: "365",
+            respiesta2: "366",
+            respiesta3: "364",
+            respiesta4: "360",
+            correcta: 2
 
         },
         pregunta4: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "4",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuántos anillos hay en la bandera olímpica?",
+            respiesta1: "7",
+            respiesta2: "3",
+            respiesta3: "4",
+            respiesta4: "5",
+            correcta: 4
 
         },
         pregunta5: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "5",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Quién es el fundador de Facebook?",
+            respiesta1: "Jeff Bezos",
+            respiesta2: "Bill Gates",
+            respiesta3: "Steve Jobs",
+            respiesta4: "Mark Zuckerberg",
+            correcta: 2
 
         }
 
@@ -102,48 +102,148 @@ export const preguntas = [{
     },
     nivel3: {
         pregunta1: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "1",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuál es el libro sagrado del Islam?",
+            respiesta1: "La Biblia",
+            respiesta2: "El libro Mormon",
+            respiesta3: "El Corán",
+            respiesta4: "El Kojiki",
+            correcta: 3
 
         },
         pregunta2: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "2",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Con qué moneda pagas si vas a Japón?",
+            respiesta1: "El yen",
+            respiesta2: "El Won",
+            respiesta3: "El Renminbi",
+            respiesta4: "La Libra Exterlina",
+            correcta: 1
 
         },
         pregunta3: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "3",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuántas películas de Harry Potter se han hecho?",
+            respiesta1: "5",
+            respiesta2: "8",
+            respiesta3: "1",
+            respiesta4: "7",
+            correcta: 2
 
         },
         pregunta4: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "4",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Qué animal contagió a los humanos en la pandemia de peste negra?",
+            respiesta1: "Ratas",
+            respiesta2: "Mosquitos",
+            respiesta3: "Pulgas",
+            respiesta4: "Perros",
+            correcta: 3
 
         },
         pregunta5: {
-            pregunta: "cuantos años tienes?",
-            respiesta1: "5",
-            respiesta2: "20",
-            respiesta3: "40",
-            respiesta4: "28",
-            correcta: "28"
+            pregunta: "¿Cuál ha sido el futbolista más caro de la historia?",
+            respiesta1: "Messi",
+            respiesta2: "Ronaldinho",
+            respiesta3: "Cristiano Ronaldo",
+            respiesta4: "Neymar",
+            correcta: 4
+
+        }
+
+
+
+    },
+    nivel4: {
+        pregunta1: {
+            pregunta: "¿En qué continente está Siria?",
+            respiesta1: "Europa",
+            respiesta2: "Norte America",
+            respiesta3: "En Asia",
+            respiesta4: "Sur America",
+            correcta: 3
+
+        },
+        pregunta2: {
+            pregunta: "¿Cuál es el órgano más grande del cuerpo humano?",
+            respiesta1: "El Pulmon",
+            respiesta2: "La Piel",
+            respiesta3: "El intestino grueso",
+            respiesta4: "El intestino delgado",
+            correcta: 2
+
+        },
+        pregunta3: {
+            pregunta: "¿Quién fue el último faraón de Egipto?",
+            respiesta1: "Tutankamon",
+            respiesta2: "Ramsés III",
+            respiesta3: "Kefren",
+            respiesta4: "Merenptah",
+            correcta: 2
+
+        },
+        pregunta4: {
+            pregunta: "¿Cuántos huesos tiene el cuerpo humano?",
+            respiesta1: "307",
+            respiesta2: "55",
+            respiesta3: "127",
+            respiesta4: "206",
+            correcta: 4
+
+        },
+        pregunta5: {
+            pregunta: "¿Cuál es la luna más grande de Saturno?",
+            respiesta1: "Titán",
+            respiesta2: "Pandora",
+            respiesta3: "CNarvi",
+            respiesta4: "Egeon",
+            correcta: 1
+
+        }
+
+
+
+    },
+    nivel5: {
+        pregunta1: {
+            pregunta: "¿Cuál fue la primera civilización humana?",
+            respiesta1: "Los Fenicios",
+            respiesta2: "La Romana",
+            respiesta3: "La china",
+            respiesta4: "La sumeria",
+            correcta: 4
+
+        },
+        pregunta2: {
+            pregunta: "¿Dónde está Transilvania?",
+            respiesta1: "Rumanía",
+            respiesta2: "Italia",
+            respiesta3: "Artantida",
+            respiesta4: "Groenlandi",
+            correcta: 1
+
+        },
+        pregunta3: {
+            pregunta: "¿Cuántos corazones tiene un gusano de tierra?",
+            respiesta1: "1",
+            respiesta2: "5",
+            respiesta3: "3",
+            respiesta4: "No tienen",
+            correcta: 2
+
+        },
+        pregunta4: {
+            pregunta: "¿Cuándo llegó el hombre a la Luna?",
+            respiesta1: "11 de junio 2000 ",
+            respiesta2: "15 de octubre 1895",
+            respiesta3: "16 de julio de 1969",
+            respiesta4: "16 de junio de 1969",
+            correcta: 3
+
+        },
+        pregunta5: {
+            pregunta: "¿Cuántas patas tiene una araña?",
+            respiesta1: "8",
+            respiesta2: "4",
+            respiesta3: "100",
+            respiesta4: "6",
+            correcta: 1
 
         }
 
